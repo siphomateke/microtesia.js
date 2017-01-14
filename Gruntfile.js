1991a28c11bf648ba2364787ecfc95a8b20cf099
Ricardo Gladwell <ricardo@gladwell.me>
2017-01-14 19:36:36 +0000

    Initialised with basic parser
    Initialised project with basic parser that supports some microdata
    specifications. Does not yet support:
    
     * Reporting parsing errors
     * Referenced items
     * Global identifiers
    
    Initial test cases were copied from Microtesia:
    
    https://github.com/rgladwell/microtesia/blob/master/src/test/scala/microtesia/ParseSpecification.scala
    
    The initial project was generated from the following Yeoman template:
    
    https://github.com/asbjornenge/generator-microlib

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/{,*/}*.js'],
+        dest: 'dist/<%= pkg.name %>'
+      }
+    },
+
+    jshint: {
+      options: grunt.file.readJSON('.jshintrc'),
+      lib_test: {
+        src: ['lib/{,*/}*.js']
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'nyan'
+        },
+        src: ['test/*.js']
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'mochaTest', 'concat']);
+  grunt.registerTask('test', ['mochaTest']);
+
+};
